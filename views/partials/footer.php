802e9535fe61ef7bc65442a5ab9b98fc43828c04
zakkturner <zakkturner@yahoo.com>
2024-12-09 08:37:51 -0500

    starting to clean html

---
+++ views/partials/footer.php
@@ -0,0 +1,68 @@
+<!-- Footer -->
+<footer class="content-footer footer bg-footer-theme">
+    <div class="container-xxl">
+        <div
+                class="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+            <div class="text-body">
+                ©
+                <script>
+                    document.write(new Date().getFullYear());
+                </script>
+                , made with ❤️ by
+                <a href="https://themeselection.com" target="_blank" class="footer-link">ThemeSelection</a>
+            </div>
+            <div class="d-none d-lg-inline-block">
+                <a href="https://themeselection.com/license/" class="footer-link me-4" target="_blank">License</a>
+                <a href="https://themeselection.com/" target="_blank" class="footer-link me-4">More Themes</a>
+
+                <a
+                        href="https://demos.themeselection.com/sneat-bootstrap-html-admin-template/documentation/"
+                        target="_blank"
+                        class="footer-link me-4"
+                >Documentation</a
+                >
+
+                <a
+                        href="https://github.com/themeselection/sneat-html-admin-template-free/issues"
+                        target="_blank"
+                        class="footer-link"
+                >Support</a
+                >
+            </div>
+        </div>
+    </div>
+</footer>
+<!-- / Footer -->
+
+<div class="content-backdrop fade"></div>
+</div>
+<!-- Content wrapper -->
+</div>
+<!-- / Layout page -->
+</div>
+
+<!-- Overlay -->
+<div class="layout-overlay layout-menu-toggle"></div>
+</div>
+<!-- Core JS -->
+<!-- build:js assets/vendor/js/core.js -->
+
+<script src="/assets/vendor/libs/jquery/jquery.js"></script>
+<script src="/assets/vendor/libs/popper/popper.js"></script>
+<script src="/assets/vendor/js/bootstrap.js"></script>
+<script src="/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js"></script>
+<script src="/assets/vendor/js/menu.js"></script>
+
+<!-- endbuild -->
+
+<!-- Vendors JS -->
+
+<!-- Main JS -->
+<script src="/assets/js/main.js"></script>
+
+<!-- Page JS -->
+
+<!-- Place this tag before closing body tag for github widget button. -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+</body>
+</html>
